0dc7c044b1beb894cc600124fd736f6f12533492
Yang Li <liyanghehe@gmail.com>
2019-04-01 14:31:51 -0400

    add http connection

--- WeatherReport/src/rpc/LoadWeatherData.java
+++ WeatherReport/src/rpc/LoadWeatherData.java
@@ -1,6 +1,8 @@
 package rpc;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +29,18 @@ public class LoadWeatherData extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//Create a PrintWriter from response such that we can add data to response.
+		PrintWriter out = response.getWriter();
+		if (request.getParameter("username") != null) {
+			//Get the username sent from the client
+			String username = request.getParameter("username");
+            //In the output stream, add something to response body. 
+			out.print("Hello " + username);
+		}
+		// Send response back to client
+		out.flush();
+		out.close();
 	}
 
 	/**
